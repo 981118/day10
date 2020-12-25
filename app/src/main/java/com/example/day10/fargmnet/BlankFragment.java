466980cd3fd576142165b716d0770a43bf031aec
981118 <2832030776@qq.com>
2020-12-25 16:53:24 +0800

    第二次

--- app/src/main/java/com/example/day10/fargmnet/BlankFragment.java
+++ app/src/main/java/com/example/day10/fargmnet/BlankFragment.java
@@ -3,21 +3,52 @@ package com.example.day10.fargmnet;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.day10.R;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
 
 
 public class BlankFragment extends Fragment {
+
+
+    private View view;
+    private RecyclerView rcv;
+    private ArrayList<Integer> integers;
+    private MyRcvAdapter myRcvAdapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        view = inflater.inflate(R.layout.fragment_blank, container, false);
+        initView();
+        initData();
+        return view;
     }
+
+    private void initData() {
+        integers.add(R.drawable.tu2);
+        integers.add(R.drawable.tu5);
+        integers.add(R.drawable.tu2);
+        myRcvAdapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        rcv = view.findViewById(R.id.rcv);
+        rcv.setLayoutManager(new LinearLayoutManager(getContext()));
+        integers = new ArrayList<>();
+        myRcvAdapter = new MyRcvAdapter(integers,getContext());
+        rcv.setAdapter(myRcvAdapter);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/day10/fargmnet/MyRcvAdapter.java
@@ -0,0 +1,60 @@
+package com.example.day10.fargmnet;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.day10.R;
+import com.youth.banner.Banner;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
+
+public class MyRcvAdapter extends RecyclerView.Adapter {
+    private ArrayList<Integer> list;
+    private Context context;
+    private Banner ban;
+
+    public MyRcvAdapter(ArrayList<Integer> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(context).inflate(R.layout.ban, parent, false);
+        return new MyHoler1(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        MyHoler1 myHoler1= (MyHoler1) holder;
+        myHoler1.ban.setImages(list).setImageLoader(new ImageLoader() {
+            @Override
+            public void displayImage(Context context, Object path, ImageView imageView) {
+                Glide.with(context).load(path).into(imageView);
+            }
+        }).start();
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyHoler1 extends RecyclerView.ViewHolder {
+        private final Banner ban;
+        public MyHoler1(@NonNull View itemView) {
+            super(itemView);
+            ban = itemView.findViewById(R.id.ban);
+        }
+    }
+}
